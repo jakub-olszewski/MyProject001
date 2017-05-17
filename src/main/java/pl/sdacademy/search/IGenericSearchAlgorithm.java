81a826980dd642fe0527463a257efa11e640a698
sdacademy <j.b.olszewski@gmail.com>
2017-05-17 19:21:24 +0200

    Predicate example

--- src/main/java/pl/sdacademy/search/IGenericSearchAlgorithm.java
+++ src/main/java/pl/sdacademy/search/IGenericSearchAlgorithm.java
@@ -6,6 +6,6 @@ import java.util.function.Predicate;
  * http://howtodoinjava.com/java-8/how-to-use-predicate-in-java-8/
  * 
  */
-public interface IGenericSearchAlgorithm {
+public interface IGenericSearchAlgorithm<T> {
     <T extends Comparable> int search(T[] array, Predicate<T> predicate);
 }
