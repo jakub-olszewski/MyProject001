7edf5b3a8dbd31c4a3baa8d9c2ad05b0132e64fc
sdacademy <j.b.olszewski@gmail.com>
2017-05-22 20:02:33 +0200

    Stos implementacja interfejsu

---
+++ src/main/java/pl/sdacademy/stack/StackElement.java
@@ -0,0 +1,25 @@
+package pl.sdacademy.stack;
+
+public class StackElement<T> {
+
+    T element;
+    StackElement<T> previous;
+
+  public T getElement() {
+    return element;
+  }
+
+  public void setElement(T element) {
+    this.element = element;
+  }
+
+  public StackElement<T> getPrevious() {
+    return previous;
+  }
+
+  public void setPrevious(StackElement<T> previous) {
+    this.previous = previous;
+  }
+
+
+}
---
+++ src/main/java/pl/sdacademy/stack/sandbox/Stack.java
@@ -0,0 +1,37 @@
+package pl.sdacademy.stack.sandbox;
+
+import pl.sdacademy.stack.IStack;
+
+public class Stack<T> implements IStack<T> {
+
+    @Override
+    public void push(T element) {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public T pop() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public T peek() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public int size() {
+	// TODO Auto-generated method stub
+	return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+	// TODO Auto-generated method stub
+	return false;
+    }
+
+}
